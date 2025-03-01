b255d1f72239b8d9164fa8fee0c825582b8ceae3
77f315 <77f315@gmail.com>
2025-03-02 00:11:18 +0200

    reset game after it has ended

--- TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -44,6 +44,7 @@ public partial class MainWindow : Window
         if (CheckWinner())
         {
             MessageBox.Show($"{cur} won");
+            ResetBoard();
         }
     }
     private bool CheckWinner()
@@ -55,7 +56,7 @@ public partial class MainWindow : Window
             if (!string.IsNullOrEmpty(board[0, i]) && board[0, i] == board[1, i] && board[1, i] == board[2, i])
                 return true;
 
-            if (!string.IsNullOrEmpty(board[0, 0]) && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            if (!string.IsNullOrEmpty(board[i, 0]) && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
                 return true;
         }
         if (!string.IsNullOrEmpty(board[0, 0]) && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
@@ -67,5 +68,20 @@ public partial class MainWindow : Window
         
 
         return false;    
-    }    
+    }
+    private void ResetBoard()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i, j] = null;
+            }
+        }
+        foreach (Button button in grid.Children) 
+        {
+            button.Content = "";
+            button.IsEnabled = true;
+        }
+    }
 }
