e070c044a3cfbcce4b6c1bdf8721f196db5c8a6b
77f315 <77f315@gmail.com>
2025-03-02 00:02:08 +0200

    ability to win

--- TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -16,7 +16,8 @@ namespace TicTacToe;
 /// </summary>
 public partial class MainWindow : Window
 {
-    bool IsXPlaying = true;
+    private string[,] board = new string[3, 3];
+    private string cur = "o";
     public MainWindow()
     {
         InitializeComponent();
@@ -25,14 +26,48 @@ public partial class MainWindow : Window
     private void btn_Click(object sender, RoutedEventArgs e)
     {
         Button clickedButton = sender as Button;
+        int row = Grid.GetRow(clickedButton);
+        int col = Grid.GetColumn(clickedButton);
 
-        if (IsXPlaying)
+        if (cur == "x")
         {
-            clickedButton.Content = "x";
-        } else
-        {
-            clickedButton.Content = "o";
+            cur = "o";
         }
-        IsXPlaying = !IsXPlaying;
+        else
+        {
+            cur = "x";
+        }
+
+       
+        clickedButton.Content = cur;
+        board[row, col] = cur;
+        if (CheckWinner())
+        {
+            MessageBox.Show($"{cur} won");
+        }
     }
+    private bool CheckWinner()
+    {
+        
+        for (int i = 0; i < 3; i++)
+        {
+          
+            if (!string.IsNullOrEmpty(board[0, i]) && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                return true;
+
+            if (!string.IsNullOrEmpty(board[0, 0]) && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+                return true;
+        }
+        if (!string.IsNullOrEmpty(board[0, 0]) && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            return true;
+        
+
+        if (!string.IsNullOrEmpty(board[0, 2]) && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            return true;
+        
+
+        return false;
+         
+    }
+    
 }
